f01ae61505ae28f50b3c435c1117ea24e7a338cb
zored <i@ro.gl>
2020-05-26 23:52:18 +0300

    [master] Add analog pin

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
 const {Led, Board} = require("johnny-five");
 
-function main() {
-  const board = new BlinkBoard();
-  board.run();
-}
+const main = () => new AnalogPinBoard().run();
 
 class RootBoard {
+  constructor() {
+    this.board = new Board({repl: false});
+  }
   ready() {}
   exit() {}
+
   run() {
-    const board = new Board({repl: false});
-    board.on("ready", () => {
+    this.board.on("ready", () => {
       this.ready()
-      board.on("exit", () => this.exit());
+      this.board.on("exit", () => this.exit());
     });
   }
 }
 
-class BlinkBoard extends RootBoard {
+class LedBoard extends RootBoard {
+  constructor(ledPin = 13) {
+    super();
+    this.ledPin = ledPin;
+  }
+
   ready() {
-    this.led = new Led(13);
-    this.led.blink(1000);
+    this.led = new Led(this.ledPin);
   }
 
   exit() {
     this.led.off();
+  }
+}
+
+class BlinkBoard extends LedBoard {
+  ready() {
+    super.ready()
+    this.led.blink(1000);
+  }
+}
+
+class AnalogPinBoard extends LedBoard {
+  constructor(analogLedPin = 9) {
+    super(analogLedPin)
+  }
+
+  ready() {
+    super.ready();
+    
+    const blink = () => this.led.fadeIn(1000, () => setTimeout(() => this.led.fadeOut(1000, blink), 1000));
+    blink();
   }
 }
 
