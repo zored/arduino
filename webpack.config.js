e01da189cbc19ffd6d9cea4cd642084a42e02f8a
zored <i@ro.gl>
2020-06-07 14:09:05 +0300

    [master] Pretty webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,4 @@
 const path = require('path');
-
-const babel = {
-  plugins: [
-    ['transform-object-rest-spread', {useBuiltIns: true}],
-    'check-es2015-constants',
-    'transform-es2015-block-scoped-functions',
-    ['transform-es2015-block-scoping', {throwIfClosureRequired: true}],
-    'transform-es2015-spread',
-    'transform-es2015-destructuring',
-    'transform-es2015-parameters',
-    'transform-es2015-shorthand-properties',
-    ['fast-async', {spec: true}],
-    'transform-es2015-classes',
-  ],
-};
-
 module.exports = {
   entry: [
     './sketch/ts/_config/promise-fix.js',
@@ -31,7 +15,20 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: babel,
+          options: {
+            plugins: [
+              ['transform-object-rest-spread', {useBuiltIns: true}],
+              'check-es2015-constants',
+              'transform-es2015-block-scoped-functions',
+              ['transform-es2015-block-scoping', {throwIfClosureRequired: true}],
+              'transform-es2015-spread',
+              'transform-es2015-destructuring',
+              'transform-es2015-parameters',
+              'transform-es2015-shorthand-properties',
+              ['fast-async', {spec: true}],
+              'transform-es2015-classes',
+            ],
+          },
         },
       },
     ],
